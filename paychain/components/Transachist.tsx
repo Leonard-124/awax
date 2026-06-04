5f3c7c5f560e352621f28bd82e5764f5b226bf83
Leonard-124 <leonardoduor91@gmail.com>
2026-06-05 02:14:01 +0300

    Changes made to paychain app

---
+++ paychain/components/Transachist.tsx
@@ -0,0 +1,19 @@
+
+
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Transachist = () => {
+  return (
+    <View className='bg-[#dfdddd] h-20'>
+      <View>
+        <Text>You have no recent transactions</Text>
+      </View>
+    </View>
+  )
+}
+
+export default Transachist
+
+const styles = StyleSheet.create({})
