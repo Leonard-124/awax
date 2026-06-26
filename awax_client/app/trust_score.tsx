6dc9dc494a5d9b7dd19fca2d22964b047cd0ec31
Leonard-124 <leonardoduor91@gmail.com>
2026-06-26 18:24:29 +0300

    Changes made to awax repo

--- awax_client/app/trust_score.tsx
+++ awax_client/app/trust_score.tsx
@@ -1,46 +1,62 @@
 
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+// import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+// import React from 'react'
+// import { router } from 'expo-router'
+
+// import { useState, useEffect } from 'react'
+// import { authedFetch, clearTokens, getRefreshToken } from './(Auth)/auth'
+
+// const BASE_URL = "https://paychain-backend.onrender.com"
+
+// interface meP {
+//   id: number,
+//   username: string
+// }
+
+// const trust_score = ({id, username}: meP) => {
+//   const [loading, setLoading] = useState(true)
+//   const [error, setError] = useState("")
+//   const [data, setData]
+
+//   useEffect(() => { fetchUser(); }, [])
+
+//   async function fetchUser() {
+//     setLoading(true);
+//     setError("");
+//     try {
+//       const res = await authedFetch("/me");
+//       if(!res.ok) {setError("Failed to load profile. Please try again"); return; }
+//     }
+//   }
+//   return (
+//     <View className='bg-[#eaf3eb] text-center text-xl'> 
+//       {/* <Text>Hello: {" "} View your trust score</Text>
+//       <TouchableOpacity className='text-center underline' onPress={() => router.push("/(Tabs)/home")}>
+//         Go back
+//       </TouchableOpacity> */}
+//       <View className='min-h-52 bg-[#f5f5f5]'>
+//        <Text></Text>
+//       </View>
+//     </View>
+//   )
+// }
+
+// export default trust_score;
+
+// const styles = StyleSheet.create({})
+///////////////////////////////////////////////
+
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { router } from 'expo-router'
 
-import { useState, useEffect } from 'react'
-import { authedFetch, clearTokens, getRefreshToken } from './(Auth)/auth'
-
-const BASE_URL = "https://paychain-backend.onrender.com"
-
-interface meP {
-  id: number,
-  username: string
-}
-
-const trust_score = ({id, username}: meP) => {
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState("")
-  const [data, setData]
-
-  useEffect(() => { fetchUser(); }, [])
-
-  async function fetchUser() {
-    setLoading(true);
-    setError("");
-    try {
-      const res = await authedFetch("/me");
-      if(!res.ok) {setError("Failed to load profile. Please try again"); return; }
-    }
-  }
+const trust_score = () => {
   return (
-    <View className='bg-[#eaf3eb] text-center text-xl'> 
-      {/* <Text>Hello: {" "} View your trust score</Text>
-      <TouchableOpacity className='text-center underline' onPress={() => router.push("/(Tabs)/home")}>
-        Go back
-      </TouchableOpacity> */}
-      <View className='min-h-52 bg-[#f5f5f5]'>
-       <Text></Text>
-      </View>
+    <View>
+      <Text>trust_score</Text>
     </View>
   )
 }
 
-export default trust_score;
+export default trust_score
 
 const styles = StyleSheet.create({})
